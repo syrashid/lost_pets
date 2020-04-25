575c57c0fbd4e8f51b90576af89dc848bb065795
Sy Rashid <sy.rashid.11@gmail.com>
2020-04-25 18:54:33 +1000

    FEAT: Populated seed file with pets

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
- Pet.create(name: 'dory', address: 'ocean', species: 'fish', found_on: Date.new(2020,3,21))
+Pet.create(name: 'dory', address: 'ocean', species: 'fish', found_on: Date.new(2020,3,21))
+
+Pet.create(name: 'nemo', address: 'ocean', species: 'fish', found_on: Date.new(2020,3,21))
+
+Pet.create(name: 'salem', address: 'burke', species: 'cat', found_on: Date.new(2020,3,14))
+
+Pet.create(name: 'wishbone', address: 'melbourne', species: 'dog', found_on: Date.new(2020,3,14))
+
+Pet.create(name: 'clifford', address: 'sydney', species: 'dog', found_on: Date.today)
