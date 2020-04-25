373d2a687779e971f8374e34f4cd59888a51acdd
Sy Rashid <sy.rashid.11@gmail.com>
2020-04-25 16:41:18 +1000

    ARCH: Created model Pets

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ Pet.create(name: 'dory', address: 'ocean', species: 'fish', found_on: Date.new(2020,3,21))
