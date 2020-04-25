62b35e4a17138d25f1ab785c842d42d92320b8e8
Sy Rashid <sy.rashid.11@gmail.com>
2020-04-25 23:29:29 +1000

    FIX: Made index page roots page on index

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
-  # This is for my index
-  get "pets", to: "pets#index"
+  # For display all pets user story
+  root to: "pets#index"
 
+  # For my creating a new pet user story
   get "pets/new", to: "pets#new"
   post "pets", to: "pets#create"
 
+  # For displaying info on a singular pet story
   get "pets/:id", to: "pets#show", as: :pet
 
+  # For editing and updating a pet
   get "pets/:id/edit", to: "pets#edit", as: :edit_pet
   patch "pets/:id", to: "pets#update"
 
+  # For removing a pet from the site
   delete "pets/:id", to: "pets#destroy"
 end
