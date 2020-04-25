46ddd51dde3608b40d9c8f85842b3e14b535d04f
Sy Rashid <sy.rashid.11@gmail.com>
2020-04-26 00:04:08 +1000

    FEAT: Implemented a simple test for an instance method for pet

--- test/models/pet_test.rb
+++ test/models/pet_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class PetTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#found_days_ago returns an Integer" do
+    snoopy = pets(:snoopy) # <-- uses the `test/fixtures/pets.yml` to create the instance
+    assert_instance_of Integer, snoopy.found_days_ago
+  end
 end
