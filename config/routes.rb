63274d2bfe429b5a4f2a94c1994c7a43d691b1cd
Sy Rashid <sy.rashid.11@gmail.com>
2020-04-25 17:00:10 +1000

    FEAT: Added index page for pets

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # This is for my index
+  get "pets", to: "pets#index"
+
+  get "pets/:id", to: "pets#show", as: :pet
 end
